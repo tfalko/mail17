ec9fddc703dff927e1d367179378b96f10b041f2
Tomasz Falkowski <t.falkowski@hotmail.com>
2020-08-31 14:14:28 +0200

    Initial commit

---
+++ src/main/java/pl/tfalkowski/mail17/Mail17Application.java
@@ -0,0 +1,13 @@
+package pl.tfalkowski.mail17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mail17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mail17Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tfalkowski/mail17/Mail17ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tfalkowski.mail17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mail17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
